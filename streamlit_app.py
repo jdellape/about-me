c8250600a831485efb3e718226e8a441eaa04e68
John Dellape <dellapejw@gmail.com>
2022-10-19 16:42:37 -0400

    more links

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,8 @@ st.info('Data professional with a passion for solving meaningful information coo
 
 icon_size = 20
 
-#st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-#st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-#st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
+st_button('', 'https://jdellape-streamlit-portfolio-app-1ay2ev.streamlitapp.com/', 'View my Streamlit Projects', icon_size)
+st_button('', 'https://github.com/jdellape', 'View my GitHub', icon_size)
 st_button('', 'https://drive.google.com/file/d/12q8t23CVe2gRy8giusX-Q83d2ta2wFYS/view', 'View my Resume', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/john-dellape/', 'Follow me on LinkedIn', icon_size)
-st_button('twitter', 'https://twitter.com/JohnDellape', 'Follow me on Twitter', icon_size)
-#input a button to view resume here
-#st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-#st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/john-dellape/', 'Find me on LinkedIn', icon_size)
+st_button('twitter', 'https://twitter.com/JohnDellape', 'Find me on Twitter', icon_size)
