222eafa73382d5259356f43e7c37cba3887e2d43
John Dellape <dellapejw@gmail.com>
2022-10-19 16:51:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ icon_size = 20
 
 st_button('', 'https://jdellape-streamlit-portfolio-app-1ay2ev.streamlitapp.com/', 'View my Streamlit Projects', icon_size)
 st_button('', 'https://github.com/jdellape', 'View my GitHub', icon_size)
-st_button('', 'https://drive.google.com/file/d/12q8t23CVe2gRy8giusX-Q83d2ta2wFYS/view', 'View my Resume', icon_size)
+st_button('', 'https://drive.google.com/file/d/1nELjAIJFVcW-PGYB-yCiscOAUi14_smI/view', 'View my Resume', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/john-dellape/', 'Find me on LinkedIn', icon_size)
 st_button('twitter', 'https://twitter.com/JohnDellape', 'Find me on Twitter', icon_size)
